1ce58e7e5e20955ab90fe1a62bbc32dc655f455e
Sara Saleem <8222963+ssaleem@users.noreply.github.com>
2018-11-05 09:51:35 -0500

    Move game panel to the side of game canvas
    Redesigning layout with a larger game panel on the side instead
    of top; creating more vertical space to include game instructions
    and possible spirite choices

--- js/engine.js
+++ js/engine.js
@@ -26,7 +26,9 @@ var Engine = (function(global) {
 
   canvas.width = 505;
   canvas.height = 606;
-  doc.body.appendChild(canvas);
+  var container = doc.querySelector('.game-canvas');
+  container.appendChild(canvas)
+  // doc.body.appendChild(canvas);
 
   /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
